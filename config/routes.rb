75dd5758587c2a068e0aee1c9218cbab82b62423
Tsubasa Masaki <46633705+Tsubasa08@users.noreply.github.com>
2023-03-05 17:37:46 +0900

    Revert "C2s05 (#1)"
    This reverts commit 7392d3988542e0dde99e9c16b0f8b183ca9db9e9.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  resources :games
-  resources :teams
-  root to: 'welcome#index'
-  resource :prediction, only: [:new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
